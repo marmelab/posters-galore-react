1faf4487658fd054a34c418ba67f540cc6815b7c
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-07-15 17:24:30 +0200

    Updates

---
+++ precache-manifest.78375f884548bd9eaf2f5527c99cd803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bae21057283c91ee20d801e73605e56",
+    "url": "/marmelab/posters-galore-react/index.html"
+  },
+  {
+    "revision": "090d12ece41596f63307",
+    "url": "/marmelab/posters-galore-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "31836eae6029ab145a11",
+    "url": "/marmelab/posters-galore-react/static/js/2.e000d426.chunk.js"
+  },
+  {
+    "revision": "090d12ece41596f63307",
+    "url": "/marmelab/posters-galore-react/static/js/main.e97812d2.chunk.js"
+  },
+  {
+    "revision": "24987f78d132f9bd1311",
+    "url": "/marmelab/posters-galore-react/static/js/runtime~main.b2a59e7e.js"
+  }
+]);
